dd863be65079c14191946c7a20f20e00f0f72645
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2025-06-12 10:27:43 -0300

    Implementado integração com a OPEN AI

--- src/01_Integrating.NET_AI_OpenAI/DotNetAI/DotNetAI/Extensions/OpenAIExtensions.cs
+++ src/01_Integrating.NET_AI_OpenAI/DotNetAI/DotNetAI/Extensions/OpenAIExtensions.cs
@@ -1,6 +1,24 @@
-﻿namespace DotNetAI.Extensions
+﻿using OpenAI;
+
+namespace DotNetAI.Extensions
 {
-    public class OpenAIExtensions
+    public static class OpenAIExtensions
     {
+        public static WebApplicationBuilder AddOpenAI(this WebApplicationBuilder builder)
+        {
+            // var apiKey = builder.Configuration["OpenAI:Key"];
+            var apiKey = Environment.GetEnvironmentVariable("OPEN_AI_API_KEY");
+
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                throw new InvalidOperationException("OpenAI API Key is not set.");
+            }
+
+            var openAIClient = new OpenAIClient(apiKey);
+
+            builder.Services.AddSingleton(openAIClient);
+
+            return builder;
+        }
     }
 }
--- src/01_Integrating.NET_AI_OpenAI/DotNetAI/DotNetAI/Program.cs
+++ src/01_Integrating.NET_AI_OpenAI/DotNetAI/DotNetAI/Program.cs
@@ -1,8 +1,11 @@
+using DotNetAI.Extensions;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.AddOpenAI();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
