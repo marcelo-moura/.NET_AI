7131e3b1421a43f9df55a6d232cd33c1e505136a
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2025-06-13 10:32:02 -0300

    Documentando a API com Scalar e OpenAPI

--- src/01_Integrating.NET_AI_OpenAI/DotNetAI/DotNetAI/Program.cs
+++ src/01_Integrating.NET_AI_OpenAI/DotNetAI/DotNetAI/Program.cs
@@ -21,7 +21,31 @@ builder.Services.AddCors(options => options.AddDefaultPolicy(builder =>
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>
+{
+    options.AddDocumentTransformer((document, context, _) =>
+    {
+        document.Info = new()
+        {
+            Title = ".NET AI API",
+            Version = "v1",
+            Description = "This API provides AI-based features such as chat, image generation, recipe creation and audio transcription.",
+            Contact = new()
+            {
+                Name = "Marcelo Moura",
+                Email = "",
+                Url = new Uri("https://br.linkedin.com/in/marcelo-henrique-9b084b165")
+            },
+            License = new()
+            {
+                Name = "Apache 2 License",
+                Url = new Uri("https://github.com/marcelo-moura")
+            },
+            TermsOfService = new Uri("https://github.com/marcelo-moura")
+        };
+        return Task.CompletedTask;
+    });
+});
 
 var app = builder.Build();
 
@@ -31,7 +55,12 @@ app.UseCors();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.MapScalarApiReference();
+    app.MapScalarApiReference(options =>
+    {
+        options.Title = ".NET AI API";
+        options.Theme = ScalarTheme.Default;
+        options.DefaultHttpClient = new(ScalarTarget.Http, ScalarClient.Http11);
+    });
 }
 
 app.UseAuthorization();
