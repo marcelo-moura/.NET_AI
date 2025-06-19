b33c8a8cbb3c85fcfe9a3ab52a0a6f4f56fb6760
Marcelo Henrique <marcelohenriquea.moura@outlook.com>
2025-06-18 21:07:19 -0300

    Implementação da TranscriptionController

---
+++ src/DotNetAI.API/Controllers/TranscriptionController.cs
@@ -0,0 +1,36 @@
+﻿using DotNetAI.API.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetAI.API.Controllers
+{
+    [ApiController]
+    [Route("ai")]
+    public class TranscriptionController : ControllerBase
+    {
+        private readonly TranscriptionService _transcriptionService;
+
+        public TranscriptionController(TranscriptionService transcriptionService)
+        {
+            _transcriptionService = transcriptionService;
+        }
+
+        [HttpPost("transcribe")]
+        public async Task<IActionResult> TranscribeAudio([FromForm] IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+            {
+                return BadRequest("No file uploaded or file is empty!");
+            }
+
+            try
+            {
+                var transcription = await _transcriptionService.TranscribeAudioAsync(file);
+                return Ok(transcription);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "An error occurred while processing your audio file.");
+            }
+        }
+    }
+}
